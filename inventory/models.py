d77f9fbbf0c57d7debdfee4d028bc10bae4eabd2
Luis Bacelar <lmbacelar@gmail.com>
2018-07-30 17:55:06 +0100

    Added string representations of Make and MakeModel

--- inventory/models.py
+++ inventory/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Make(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class MakeModel(models.Model):
     make = models.ForeignKey(Make, on_delete=models.CASCADE)
@@ -12,4 +15,6 @@ class MakeModel(models.Model):
     class Meta:
         unique_together = ('make', 'model')
 
+    def __str__(self):
+        return f'{self.make} {self.model}'
 
