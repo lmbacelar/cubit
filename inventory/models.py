f96cc1622cc853f34a9e3991eb2ef2c78a6feeb3
Luis Bacelar <lmbacelar@gmail.com>
2018-07-30 17:41:22 +0100

    Added Make MakeModel models

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Make(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class MakeModel(models.Model):
+    make = models.ForeignKey(Make, on_delete=models.CASCADE)
+    model = models.CharField(max_length=255, blank=True)
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        unique_together = ('make', 'model')
+
+
