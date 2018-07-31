2a0f19b1c66f47f896a19cd1a68c469feeffff99
Luis Bacelar <lmbacelar@gmail.com>
2018-07-31 19:01:59 +0100

    Added Item to Django Admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Make, MakeModel
+from .models import Make, MakeModel, Item
 
 admin.site.register(Make)
 admin.site.register(MakeModel)
+admin.site.register(Item)
