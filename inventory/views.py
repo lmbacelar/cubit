9e61b041be4731e0341423915a14c7e3b392745f
Luis Bacelar <lmbacelar@gmail.com>
2018-07-31 23:35:57 +0100

    Using default context and template names

--- inventory/views.py
+++ inventory/views.py
@@ -4,13 +4,8 @@ from .models import Item
 
 
 class IndexView(generic.ListView):
-    template_name = 'inventory/index.html'
-    context_object_name = 'latest_item_list'
-
-    def get_queryset(self):
-        return Item.objects.order_by('-created_at')[:5]
+    model = Item
 
 
 class DetailView(generic.DetailView):
     model = Item
-    template_name = 'inventory/detail.html'
