24bea9eaedfc947785267852daa6a5413064b18c
Luis Bacelar <lmbacelar@gmail.com>
2018-07-31 19:21:02 +0100

    Added Item detail view

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
         path('', views.index, name='index'),
+        path('<int:item_id>', views.detail, name='detail'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello world! You're at the Inventory index ...")
+
+def detail(request, item_id):
+    return HttpResponse("You're looking at Item %s." % item_id)
