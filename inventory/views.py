114af7225e4b5927749bf0fd361956e56d06847b
Luis Bacelar <lmbacelar@gmail.com>
2018-07-31 21:10:51 +0100

    Rendering index view using index.html template

--- inventory/views.py
+++ inventory/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Item
 
 
 def index(request):
     latest_item_list = Item.objects.order_by('-created_at')[:5]
-    output = 'Currently on Inventory: ' + ','.join([i.__str__() for i in latest_item_list])
-    return HttpResponse(output)
+    context = { 'latest_item_list': latest_item_list }
+    return render(request, 'inventory/index.html', context)
+
 
 def detail(request, item_id):
     return HttpResponse("You're looking at Item %s." % item_id)
