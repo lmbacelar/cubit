a568773a38f17945b021f955d7bd373ecfe68585
Luis Bacelar <lmbacelar@gmail.com>
2018-07-31 23:21:00 +0100

    Nnamespaced Inventory urls

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'inventory'
 urlpatterns = [
         path('', views.index, name='index'),
         path('<int:item_id>/', views.detail, name='detail'),
