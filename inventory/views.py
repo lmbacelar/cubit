164b0d119aee0a6acf4de1d48de3008fbe52bd59
Luis Bacelar <lmbacelar@gmail.com>
2018-07-31 19:37:34 +0100

    Listing Items on index view

--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 
+from .models import Item
+
+
 def index(request):
-    return HttpResponse("Hello world! You're at the Inventory index ...")
+    latest_item_list = Item.objects.order_by('-created_at')[:5]
+    output = 'Currently on Inventory: ' + ','.join([i.__str__() for i in latest_item_list])
+    return HttpResponse(output)
 
 def detail(request, item_id):
     return HttpResponse("You're looking at Item %s." % item_id)
