39d92554895ba263699660275199bc90b805f8be
Luis Bacelar <lmbacelar@gmail.com>
2018-08-02 00:40:59 +0100

    Added user authentication urls

--- cubit/urls.py
+++ cubit/urls.py
@@ -20,3 +20,8 @@ urlpatterns = [
     path('inventory/', include('inventory.urls')),
     path('admin/', admin.site.urls),
 ]
+
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
