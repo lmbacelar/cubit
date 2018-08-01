d27d1ded6c715dfeac72cce71601e45d4602e6eb
Luis Bacelar <lmbacelar@gmail.com>
2018-08-01 23:19:17 +0100

    Renamed ItemListView and ItemDetailView. Added index view.

--- inventory/views.py
+++ inventory/views.py
@@ -1,11 +1,26 @@
+from django.shortcuts import render
 from django.views import generic
 
-from .models import Item
+from .models import Make, MakeModel, Item
 
 
-class IndexView(generic.ListView):
+def index(request):
+    """View function for home page of site."""
+
+    num_makes = Make.objects.all().count()
+    num_models = MakeModel.objects.all().count()
+    num_items = Item.objects.all().count()
+    context = {
+        'num_makes': num_makes,
+        'num_models': num_models,
+        'num_items': num_items,
+    }
+    return render(request, 'index.html', context=context)
+
+
+class ItemListView(generic.ListView):
     model = Item
 
 
-class DetailView(generic.DetailView):
+class ItemDetailView(generic.DetailView):
     model = Item
