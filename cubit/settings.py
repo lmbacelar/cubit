eee7b85309d67e36b4ad6c64e400eee387f44c56
Luis Bacelar <lmbacelar@gmail.com>
2018-08-02 00:40:10 +0100

    Added ./templates to DIRS. Added url to redirect after login.

--- cubit/settings.py
+++ cubit/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'cubit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,3 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/inventory/'
